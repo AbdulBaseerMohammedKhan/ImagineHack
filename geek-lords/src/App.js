9d3d6689019b7d63e85e832994e546b16801e6e6
Knightmare-sama <shahbaaz23khan@gmail.com>
2021-08-21 12:48:25 +0530

    added react buggy code

--- geek-lords/src/App.js
+++ geek-lords/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+export class App extends Component {
+  responseGoogle = (response) => {
+    console.log(response);
+    console.log(response.profileObj);
+  };
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+          clientId="276655996971-tqcgsfqvun9d061loj9t47demuhbqb9l.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
